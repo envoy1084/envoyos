c1bf86a6d547b8ad131c62c9501ea02f431e205c
Vedant Chainani <vedantchainani1084@gmail.com>
2026-06-21 21:05:02 +0530

    chore: add integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import cloudflare from '@astrojs/cloudflare';
+
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), mdx(), sitemap()],
+  adapter: cloudflare(),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
